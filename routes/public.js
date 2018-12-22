df8777e520f6970fc41a48bdbb00569520095eed
igolinin@gmail.com <igolinin@gmail.com>
2018-12-22 07:03:28 +0100

    get user

--- routes/public.js
+++ routes/public.js
@@ -3,7 +3,9 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.get("/user/:email", async (req, res) => {
-  res.send("ok get");
+  const email = req.params.email;
+  const result = User.findOne({ email: email });
+  res.send(result);
 });
 router.put("/user", (req, res) => {
   res.send("ok put");
